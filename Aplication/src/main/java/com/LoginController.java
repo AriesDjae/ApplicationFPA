e6ccd30ac600af67ea70e737510843398ca9c4da
Muhammad Aries Djaenuri <ariesdjaenuri24@gmail.com>
2024-06-27 21:28:27 +0700

    Add files via upload

---
+++ Aplication/src/main/java/com/LoginController.java
@@ -0,0 +1,47 @@
+package main.java.com;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void handleLoginAction(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (DatabaseHelper.isValidUser(username, password)) {
+            showAlert(Alert.AlertType.INFORMATION, "Login Successful!", "Welcome, " + username + "!");
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Login Failed", "Incorrect username or password.");
+        }
+    }
+
+    @FXML
+    protected void handleRegisterAction(ActionEvent event) {
+        try {
+            MainApp.showRegisterScene();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
+
+
---
+++ Aplication/src/main/java/com/MainApp.java
@@ -0,0 +1,41 @@
+package main.java.com;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        MainApp.primaryStage = primaryStage;
+        showLoginScene();
+    }
+
+    public static void showLoginScene() throws Exception {
+        Parent loginRoot = FXMLLoader.load(MainApp.class.getResource("/com/login.fxml"));
+        Scene loginScene = new Scene(loginRoot, 300, 200);
+        primaryStage.setTitle("Login Form");
+        primaryStage.setScene(loginScene);
+        loginScene.getStylesheets().add(MainApp.class.getResource("/com/login.css").toExternalForm());
+        primaryStage.show();
+    }
+
+    public static void showRegisterScene() throws Exception {
+        Parent registerRoot = FXMLLoader.load(MainApp.class.getResource("/com/register.fxml"));
+        Scene registerScene = new Scene(registerRoot, 400, 300);
+        primaryStage.setTitle("Register Form");
+        primaryStage.setScene(registerScene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
